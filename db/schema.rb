5faa1a5148ad37e0841d909d4ae0b1bc5a22cfe6
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-02-17 12:14:43 -0500

    Added eeo_enabled column to applicationDrafts table, with default value of true

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229155500) do
+ActiveRecord::Schema.define(version: 20160217171150) do
 
   create_table "application_drafts", force: :cascade do |t|
     t.integer  "application_template_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id",                 limit: 4
+    t.boolean  "eeo_enabled",                       default: true
   end
 
   create_table "application_records", force: :cascade do |t|
